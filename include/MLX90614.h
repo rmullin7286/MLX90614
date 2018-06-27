25fe9862bf39190c3c16ca419c775c163ef06ab4
Ryan Mullin <ryan.mullin@wsu.edu>
2018-06-27 16:42:45 -0700

    Changed MLX90614_I2CAddress to MLX90614_I2CADDRESS

--- include/MLX90614.h
+++ include/MLX90614.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <stdint.h>
 
-#define MLX90614_I2CAddress 0x5A
+#define MLX90614_I2CADDRESS 0x5A
 
 float MLX90614_ReadAmbientF(uint8_t address);
 float MLX90614_ReadAmbientC(uint8_t address);
